adcd6d880843a1799cb93ae6429de2f223ccc579
Nenad Ivkovic <nesaeco@gmail.com>
2022-04-28 22:05:35 +0200

    Landing page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    redirectTo: '',
+  },
+  {
+    path: '',
+    loadChildren: () => import('./core/components/landing-page/landing-page.module').then(m => m.LandingPageModule)
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules,
+    anchorScrolling: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
