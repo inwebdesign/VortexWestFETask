6473e1c9f3c3f2f2bdfecb07dacaafdca5939c6f
Nenad Ivkovic <nesaeco@gmail.com>
2022-05-05 08:58:50 +0200

    Progress handle refactor

---
+++ src/app/layout/pages/library/progress.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { finalStep, getSubgenresRequest, increaseProgress } from 'src/app/store/actions/shared.actions';
+import { getCurrentStep, getGenreId, getSubGenreId, isNewSubgenreAdded } from 'src/app/store/selectors/shared.selectors';
+import { sharedAppState } from 'src/app/store/state';
+import { GenreService } from './genre.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProgressService {
+  selectedGenreId$!: Subscription;
+  selectedSubgenreId$!: Subscription;
+  currentStep$!: Subscription
+  isNewSubgenreAdded = false
+  genreId!: number;
+  subgenreId!: number;
+  currentStep = 0
+
+  constructor(private store: Store<sharedAppState>, private router: Router, private genreService: GenreService) {
+    this.store.select(isNewSubgenreAdded).subscribe(res => this.isNewSubgenreAdded = res.newSubgenre)
+    this.selectedGenreId$ = this.store.select(getGenreId).subscribe(res => this.genreId = res.id)
+    this.selectedSubgenreId$ = this.store.select(getSubGenreId).subscribe(res => this.subgenreId = res.id)
+    this.currentStep$ = this.store.select(getCurrentStep).subscribe(res => this.currentStep = res)
+  }
+
+  navigateToNextStep() {
+    this.store.dispatch(increaseProgress());
+    if(this.isNewSubgenreAdded) {
+      this.router.navigateByUrl('/library/new')
+      return
+    }
+    if(this.subgenreId) {
+      this.store.dispatch(finalStep({finalStep: true}))
+      if (this.genreService.descriptionRequired) this.genreService.setDescriptionRequirement(this.genreService.descriptionRequired, true)
+      this.router.navigateByUrl('/library/info')
+      return
+    }
+    if (this.genreId) {
+      this.store.dispatch(getSubgenresRequest({id: this.genreId }))
+      return
+    }
+   }
+
+   get step() {
+     return this.currentStep
+   }
+
+}
--- src/app/shared/buttons/buttons.component.ts
+++ src/app/shared/buttons/buttons.component.ts
@@ -1,11 +1,10 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { Observable, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { GenreService } from 'src/app/layout/pages/library/genre.service';
+import { ProgressService } from 'src/app/layout/pages/library/progress.service';
 import { CategorySelected, NewSubgenre } from 'src/app/models/genres';
-import { finalStep, getSubgenresRequest, increaseProgress } from 'src/app/store/actions/shared.actions';
-import { getCurrentStep, getGenreId, getSubGenreId, isCategorySelected, isFinalStep, isNewSubgenreAdded } from 'src/app/store/selectors/shared.selectors';
+import { isCategorySelected, isFinalStep, isNewSubgenreAdded } from 'src/app/store/selectors/shared.selectors';
 import { sharedAppState } from 'src/app/store/state';
 
 @Component({
@@ -13,53 +12,20 @@ import { sharedAppState } from 'src/app/store/state';
   templateUrl: './buttons.component.html',
   styleUrls: ['./buttons.component.scss']
 })
-export class ButtonsComponent implements OnDestroy {
+export class ButtonsComponent {
   isCategorySelected$!: Observable<CategorySelected>
   isNewSubgenre$!: Observable<NewSubgenre>
-  isNewSubgenreAdded = false
-  selectedGenreId$!: Subscription;
-  selectedSubgenreId$!: Subscription;
-  genreId!: number;
-  subgenreId!: number;
-  currentStep = 0
   finalStep$!: Observable<boolean>
-  currentStep$!: Subscription
 
-  constructor(private store: Store<sharedAppState>, private genreService: GenreService, private route: Router) { 
+  constructor(private store: Store<sharedAppState>, private genreService: GenreService, private progressService: ProgressService) { 
     this.isCategorySelected$ = this.store.select(isCategorySelected)
     this.isNewSubgenre$ = this.store.select(isNewSubgenreAdded)
-    this.store.select(isNewSubgenreAdded).subscribe(res => this.isNewSubgenreAdded = res.newSubgenre)
-    this.selectedGenreId$ = this.store.select(getGenreId).subscribe(res => this.genreId = res.id)
-    this.selectedSubgenreId$ = this.store.select(getSubGenreId).subscribe(res => this.subgenreId = res.id)
     this.finalStep$ = this.store.select(isFinalStep)
-    this.currentStep$ = this.store.select(getCurrentStep).subscribe(res => this.currentStep = res)
   }
   get gnrService() {
     return this.genreService;
   }
-  navigateToNextStep() {
-    this.store.dispatch(increaseProgress());
-    if(this.isNewSubgenreAdded) {
-      this.route.navigateByUrl('/library/new')
-      return
-    }
-    if(this.subgenreId) {
-      this.store.dispatch(finalStep({finalStep: true}))
-      if (this.genreService.descriptionRequired) this.genreService.setDescriptionRequirement(this.genreService.descriptionRequired, true)
-      this.route.navigateByUrl('/library/info')
-      return
-    }
-    if (this.genreId) {
-      this.store.dispatch(getSubgenresRequest({id: this.genreId }))
-      return
-    }
-   }
-
-  ngOnDestroy(): void {
-    this.selectedGenreId$.unsubscribe()
-    this.selectedSubgenreId$.unsubscribe()
-    this.currentStep$.unsubscribe()
+  get prgService() {
+    return this.progressService
   }
-
-
 }
